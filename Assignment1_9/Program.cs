eba3c47b05f7fa04b27ad560019d43b08c61cb57
YGumarov <yerassylgumarov@gmail.com>
2023-04-09 15:33:52 +0600

    Initial commit

---
+++ Assignment1_9/Program.cs
@@ -0,0 +1,18 @@
+﻿static int BinomalCoe(int n, int k)
+{
+    if (k == 0 || k == n)
+    {
+            return 1;
+    }
+    else
+    {
+            return BinomalCoe(n - 1, k - 1) + BinomalCoe(n - 1, k);
+    }
+}
+
+int n = int.Parse(Console.ReadLine());
+int k = int.Parse(Console.ReadLine());
+
+int result = BinomalCoe(n, k);
+
+Console.WriteLine(BinomalCoe(n,k));
